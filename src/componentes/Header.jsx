e899fa6f871f163e60f04cf7385d00f83f87887f
VanehSann <vanessacbsantos22@gmail.com>
2022-03-11 21:14:40 -0300

    requisito04

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -24,6 +25,10 @@ class Header extends React.Component {
     const { nameUser, loading } = this.state;
     return (
       <header data-testid="header-component">
+        <Link to="/search" data-testid="link-to-search"> Search </Link>
+        <Link to="/favorites" data-testid="link-to-favorites"> Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile"> Profile </Link>
+
         <p data-testid="header-user-name" name="inputName">
           { loading ? <Loading /> : nameUser.name }
         </p>
